ba13963f0abeabb362c0841e490f08e4df975274
NasirSultan <rainasirsultan123@gmail.com>
2024-12-16 23:23:14 +0500

    upadte

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|max:255',
+            'name' => 'required|max:285',
             
         ]);
 
